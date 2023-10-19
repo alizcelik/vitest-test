073130231a5ab1debc13069aef7063084ad59ea3
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-10-19 09:32:41 +0300

    deep merge

--- test/merge.test.ts
+++ test/merge.test.ts
@@ -25,12 +25,14 @@ test("shallow merge with overlaps", () => {
     },
     {
       github: "aliozclk",
+      twitter: "aliozclk99",
     }
   );
 
   expect(merged).toEqual({
     name: "Ali",
     github: "aliozclk",
+    twitter: "aliozclk99",
   });
 });
 
@@ -38,4 +40,29 @@ test("shallow merge with arrays", () => {
   const merged = deepMerge(["vue", "react"], ["svelte", "angular"]);
 
   expect(merged).toEqual(["vue", "react", "svelte", "angular"]);
+});
+
+test("deep merge with overlaps", () => {
+  const merged = deepMerge(
+    {
+      name: "Ali",
+      accounts: {
+        github: "unknown",
+      },
+    },
+    {
+      accounts: {
+        github: "aliozclk",
+        twitter: "aliozclk99",
+      },
+    }
+  );
+
+  expect(merged).toEqual({
+    name: "Ali",
+    accounts: {
+      github: "aliozclk",
+      twitter: "aliozclk99",
+    },
+  });
 });

9e5da3ae0432a96ba27acb3cb39fde88af10baf6
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-10-19 15:04:54 +0300

    fs mocking test

---
+++ src/fs.ts
@@ -0,0 +1,8 @@
+import { readFileSync, existsSync } from "fs";
+
+export function loadConfig() {
+  if (!existsSync("./config.json")) {
+    return undefined;
+  }
+  return JSON.parse(readFileSync("./config.json", "utf-8"));
+}
--- test/spying.test.ts
+++ test/spying.test.ts
@@ -5,7 +5,28 @@ test("greeting", () => {
   const spy = vi.spyOn(console, "log");
 
   greeting("Ali");
-
-  expect(spy).toBeCalledWith("Hello, Ali");
   expect(spy).toBeCalledTimes(1);
+  expect(spy).toBeCalledWith("Hello, Ali");
+
+  spy.mockReset();
+  greeting("World");
+
+  expect(spy).toBeCalledTimes(1);
+  expect(spy).toBeCalledWith("Hello, World");
+
+  expect(spy).toMatchInlineSnapshot(`
+    [MockFunction log] {
+      "calls": [
+        [
+          "Hello, World",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+  `);
 });
